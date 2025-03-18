4267901aa281b77830bd4747ac6e539e33da0a57
atBJFUyiyi <yicaihui2021@163.com>
2025-03-18 18:25:16 +0800

    vitepress修改

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import { type App } from "vue";
 import VizUI from "viz-ui-y";
 import { ElementPlusContainer } from "@vitepress-demo-preview/component";
 
-import "@vitepress-demo-preview/component/style.css";
+// import "@vitepress-demo-preview/component/style.css";
 import "viz-ui-y/dist/index.css";
 
 export default {

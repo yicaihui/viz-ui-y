56372e7f32e6d5cc99517b488d6abfdfa5d5db29
atBJFUyiyi <yicaihui2021@163.com>
2025-03-28 17:29:01 +0800

    fix:依赖

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,9 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
 import { type App } from 'vue'
 import VizUI from 'viz-ui-y'
-import { ElementPlusContainer } from 'vitepress-preview-component'
+import { ElementPlusContainer } from '@vitepress-preview/component'
 
-import 'vitepress-preview-component/style.css'
+import '@vitepress-preview/component/style.css'
 import 'viz-ui-y/dist/index.css'
 
 export default {
